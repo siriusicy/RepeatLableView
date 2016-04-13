9ff91c00490ab33de064f2311ec960f783626421
siriusicy <siriusicy@gmail.com>
2016-04-13 15:51:01 +0800

    ...
    new

---
+++ RepeatLableView/RepeatLableView.h
@@ -0,0 +1,18 @@
+//
+//  RepeatLableView.h
+//  test2
+//
+//  Created by chenjie on 16/4/13.
+//  Copyright © 2016年 chenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RepeatLableView : UIView
+
+@property (nonatomic, strong) UIFont *font;
+@property (nonatomic, strong) NSString *text; //文字
+@property (nonatomic, assign) float durationTime;
+@property (nonatomic, assign) NSUInteger repeatCount; // 循环滚动次数(为0时无限滚动)
+
+@end
